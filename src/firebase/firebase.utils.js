b68f9cce77f035fa0dd3dc70499a3bbd99d4265d
Aidan Dang <aidandang@gmail.com>
2020-08-28 11:31:03 -0700

    implemented firebase utils, included store authenticated user to the database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,9 +10,35 @@ const config = {
   projectId: "e-commerce-redux-hooks-graphql",
   storageBucket: "e-commerce-redux-hooks-graphql.appspot.com",
   messagingSenderId: "634066080703",
-  appId: "1:634066080703:web:b50ba9f7e046762b50870a",
-  measurementId: "G-8ZL4SHLLCP"
+  appId: "1:634066080703:web:7306a18838edb84950870a",
+  measurementId: "G-VM9DWD893N"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+
+    } catch (error) {
+      console.log('error creating user', error.message);
+    } 
+  }
+
+  return userRef;
+}
 
 firebase.initializeApp(config);
 
